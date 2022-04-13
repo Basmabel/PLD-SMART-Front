a160e1caa9b34cfde13b8e136a56755315aa8893
mguillevic <marie.guillevic@insa-lyon.fr>
2022-04-13 08:10:51 +0200

    ajout des screens vides

--- front_eve/App.js
+++ front_eve/App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LandingScreen from './app/screens/LandingScreen.js';
+import SignInScreen from './app/screens/SignInScreen.js';
+import SignUpScreen from './app/screens/SignUpScreen.js';
 
 export default function App() {
   return (
