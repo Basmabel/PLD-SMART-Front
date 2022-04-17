d33f4b3bd0e43a931b35fb980a3d336b6641e10d
mguillevic <marie.guillevic@insa-lyon.fr>
2022-04-17 08:31:02 +0200

    change but bug

--- front_eve/babel.config.js
+++ front_eve/babel.config.js
@@ -1,6 +1,12 @@
-module.exports = function(api) {
+export default function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin'],
+    env: {
+      production: {
+        plugins: ['..............*other plug ins*'],
+      },
+    },
   };
 };
