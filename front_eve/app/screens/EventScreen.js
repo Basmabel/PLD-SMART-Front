6537f9d0a8a425154d9ee34491e7aec5188fb763
swaqif02 <92268002+swaqif02@users.noreply.github.com>
2022-04-27 01:59:07 +0000

    eventscreen update

--- front_eve/app/screens/EventScreen.js
+++ front_eve/app/screens/EventScreen.js
@@ -20,21 +20,13 @@ import { FlatList } from "native-base";
 
 export default function EventScreen() {
 
-  const [userInfo, setUserInfo] = React.useState(null);
   const [isLoading, setLoading] = React.useState(true);
   const [retreive, setRetreive] = React.useState(false);
   const [userId, setUserId] = React.useState("")
   const [userToken, setUserToken] = React.useState("")
-  const [eventTitle, setEventTitle] = React.useState("");
-  const [eventImage, setEventImage] = React.useState("");
-  const [creatorImage, setcreatorImage] = React.useState("");
-  const [creatorName, setcreatorName] = React.useState("");
-  const [date, setDate] = React.useState("");
-  const [nb_participants, setNbParticipants] = React.useState("");
-  const [city, setCity] = React.useState("");
-  const [listRules, setListRules] = React.useState([]);
-  const [description, setDescription] = React.useState("");
-  const [adress, setAdress] = React.useState("");
+  const [notifParticipant, setNotifParticipant] = React.useState(null)
+  const [infoEvent, setInfoEvent] = React.useState([])
+  const [participation, setParticipation] = React.useState("");
 
   var [fontsLoaded] = useFonts({
     Montserrat_400Regular,
@@ -62,13 +54,13 @@ export default function EventScreen() {
 
     retreiveData();
     if(retreive){
-  //Retrieve Data of the User to check wich page to load
-
-  //Retrieve Data of the Event takes the id of Event
-  //Data Contains EventTitle, Eventimage, CreatorName, CreatorPic, Date
-    // nb_max_people, City, list of rules, Description, Adress
       Promise.all([
         fetch('https://eve-back.herokuapp.com/getEventInfo',{
+          method: "POST",
+          headers: {'content-type': 'application/json'},
+          body: JSON.stringify({"id":eventId })
+        }),
+        fetch('https://eve-back.herokuapp.com/getEventParticipants',{
           method: "POST",
           headers: {'content-type': 'application/json'},
           body: JSON.stringify({"id":eventId })
@@ -83,87 +75,195 @@ export default function EventScreen() {
         // You would do something with both sets of data here
         data.map((item,index)=>{
           if(index==0){
-            setEventTitle(item)
+            setInfoEvent(item)
           }else if(index==1){
-            setEventImage(item)
-          }else if(index==2){
-            setcreatorName(item)
-          } else if(index==3){
-            setcreatorImage(item)
-          }else if(index==4){
-            setDate(item)
-          }else if(index==5){
-            setNbParticipants(item)
-          } else if(index==6){
-            setCity(item)
-          }else if(index==7){
-            setListRules(item)
-          }else if(index==8){
-            setDescription(item)
-          } else if(index==9){
-            setAdress(item)
-          }                  
+            setParticipation(item)
+          }             
         });
       }).catch(function (error) {
         // if there's an error, log it
         console.log(error);
       }).finally(()=> setLoading(false));
     }
-      
-      
-
   }, [retreive]);
-    }
-  
-  //Create a Participant Page that is participating
-
-  //Create a Participant Page that is not participating DONE
-
-  //Create a Participant Page that is waiting to be accepted to participate
-
-  //Create an Organizer Page
 
   const generate_non_participant_page = () =>{
 
+    const DATA = []
+    infoEvent[5].forEach(element => {
+      DATA.push({key:element})
+    });
+
+    const participation_demand = () => {}
+
+    return (
+      <SafeAreaView style={StyleSheet.container}>
+          <View >
+              <Image style = {styles.image} source={{uri: infoEvent[1]}}/>
+          </View>
+          <View>
+              <Text style = {styles.title_header}> {infoEvent[0]} </Text>
+              <Text style = {styles.regular_text}> {infoEvent[2]} </Text>
+              <Image style = {styles.profilImage} source={{uri: infoEvent[3]}}/>
+          </View>            
+          <View style = {styles.content}>
+            <FlatList
+              data = {DATA}
+              renderItem={({item})=><Text style = {styles.regular_text}>{item.key}</Text>}
+            />
+            <Text style = {styles.title_section}>Description</Text>
+            <Text style = {styles.regular_text}>{infoEvent[1]}</Text>
+            
+            <View style = {styles.button}>
+              {/* <Button title="Participate!" onPress={()=>console.log("lemme participate")}/> */}
+              {/*the status id of a participant is 3, on press the request is sent and the button becomes unvalid
+              ={()=>{
+                  const sendInputPhoneNumber = (inputText) => {setuserInfo({ ...userInfo,phoneNumber: inputText,});
+                  setVisiblePhoneNumber(false); } 
+                  }
+                }
+              
+              */}
+              <Button title = "Participate!" onPress={()=>participation_demand()}/>
+            </View>
+          </View>
+      </SafeAreaView>
+
+    )
   }
 
   const generate_participant_page = () =>{
+    const DATA = []
+    infoEvent[5].forEach(element => {
+      DATA.push({key:element})
+    });
 
+    const withdrawal_demand = () => {}
+
+    return(      
+      //add the full adress and place
+      //add list of other paticipants
+    <SafeAreaView style={StyleSheet.container}>
+      <View >
+          <Image style = {styles.image} source={{uri: infoEvent[1]}}/>
+      </View>
+      <View>
+          <Text style = {styles.title_header}> {infoEvent[0]} </Text>
+          <Text style = {styles.regular_text}> {infoEvent[2]} </Text>
+          <Image style = {styles.profilImage} source={{uri: infoEvent[3]}}/>
+      </View>            
+      <View style = {styles.content}>
+        <FlatList
+          data = {DATA}
+          renderItem={({item})=><Text style = {styles.regular_text}>{item.key}</Text>}
+        />
+        <Text style = {styles.title_section}>Description</Text>
+        <Text style = {styles.regular_text}>{infoEvent[1]}</Text>
+        
+        <View style = {styles.button}>
+          <Button title = "Withdraw" onPress={()=>withdrawal_demand()}/>
+        </View>
+      </View>
+  </SafeAreaView>)
   }
 
   const generate_organizer_page = () =>{
+        //Edit functions
+    const editTitle= () => {setVisibleTitle(true);}
+    const editImage= () => {setVisibleImage(true);}
+    const editMaxCapacity= () => {setVisibleMaxCapacity(true);}
+    const editCity= () => {setVisibleCity(true);}
+    const editPlace= () => {setVisiblePlace(true);}
+    const editAdress= () => {setVisibleAdress(true);}
+    const editDescription= () => {setVisibleDescription(true);}
 
+    //Dialog Visibility
+    const [visibleTitle, setVisibleTitle] = useState(false);
+    const [visibleImage, setVisibleImage] = useState(false);
+    const [visibleMaxCapacity, setVisibleMaxCapacity] = useState(false);
+    const [visibleCity, setVisibleCity] = useState(false);
+    const [visiblePlace, setVisiblePlace] = useState(false);
+    const [visibleAdress, setVisibleAdress] = useState(false);
+    const [visibleDescription, setVisibleDescription] = useState(false);
+
+    //Writing data
+
+    const sendInputTitle = (inputText) => {setInfoEvent({ ...infoEvent,name: inputText,});
+    setVisibleTitle(false);
+    };
+    const sendInputImage = (inputText) => {setInfoEvent({ ...infoEvent,image: inputText,});
+    setVisibleImage(false);
+    };
+    const sendInputMaxCapacity = (inputText) => {setInfoEvent({ ...infoEvent,maxcap: inputText,});
+    setVisibleMaxCapacity(false);
+    };
+    const sendInputCity = (inputText) => {setInfoEvent({ ...infoEvent,city: inputText,});
+    setVisibleCity(false);
+    };
+    const sendInputPlace = (inputText) => {setInfoEvent({ ...infoEvent,place: inputText,});
+    setVisiblePlace(false);
+    };
+    const sendInputAdress = (inputText) => {setInfoEvent({ ...infoEvent,adress: inputText,});
+    setVisibleAdress(false);
+    };
+    const sendInputDescription = (inputText) => {setInfoEvent({ ...infoEvent,description: inputText,});
+    setVisibleDescrition(false);
+    };
+
+    ////////////////////////////////
+    const DATA = []
+    infoEvent[5].forEach(element => {
+      DATA.push({key:element})
+    });
+
+    const MEMBERS = []
+    participation.forEach(element =>{
+      MEMBERS.push({userid : element[0],
+                    user_name : element[1],
+                    user_surname : element[2]})
+    });
+
+    const delete_participant = () =>{}
+
+    return(
+      <SafeAreaView style={StyleSheet.container}>
+          <View >
+              <Image style = {styles.image} source={{uri: infoEvent[1]}}/>
+          </View>
+          <View>
+              <Text style = {styles.title_header}> {infoEvent[0]} </Text>
+              <Text style = {styles.regular_text}> {infoEvent[2]} </Text>
+              <Image style = {styles.profilImage} source={{uri: infoEvent[3]}}/>
+          </View>            
+          <View style = {styles.content}>
+            <FlatList
+              data = {DATA}
+              renderItem={({item})=><Text style = {styles.regular_text}>{item.key}</Text>}
+            />
+            <Text style = {styles.title_section}>Description</Text>
+            <Text style = {styles.regular_text}>{infoEvent[1]}</Text>
+            <FlatList
+              members = {MEMBERS}
+              renderItem={({item})=><View><Text style={styles.regular_text}>{item.user_name} {item.user_surname}</Text>
+                                    <Button title="X" onPress={()=>{delete_participant()}}/></View>
+                                  }
+              />
+            <View style = {styles.button}>
+              <Button title = "Delete Event" onPress={()=>delete_event()}/>
+            </View>
+          </View>
+      </SafeAreaView>
+    )
   }
 
-
-  return (
-    <SafeAreaView style={StyleSheet.container}>
-        <View >
-            <Image style = {styles.image} source={{uri: "https://picsum.photos/id/11/200/300"}}/>
-        </View>
-        <View>
-            <Text style = {styles.title_header}> Event Title </Text>
-            <Text style = {styles.regular_text}> nb personnes </Text>
-            <Image style = {styles.profilImage} source={{uri: "https://i.pinimg.com/564x/37/03/e4/3703e4c18098c3fb6c62f452cd5f8412.jpg"}}/>
-        </View>            
-        <View style = {styles.content}>
-          <FlatList
-            data = {[{key:'gratuit'}, {key:'pets allowed'}, {key:'no party poopers'}]}
-            renderItem={({item})=><Text style = {styles.regular_text}>item.key</Text>}
-          />
-          <Text style = {styles.title_section}>Description</Text>
-          <Text style = {styles.regular_text}>Un texte est une série orale ou écrite de mots 
-                                              perçus comme constituant un ensemble cohérent, porteur 
-                                              de sens et utilisant les structures propres à une langue 
-                                              (conjugaisons, construction et association des phrases…).</Text>
-          
-          <View style = {styles.button}>
-            <Button title="Participate" onPress={()=>console.log("lemme participate")}/>
-          </View>
-        </View>
-    </SafeAreaView>
-
-  )
+  if (userId == creatorId){
+    return generate_organizer_page();
+    //the condition is wrong to check 
+  }else if(participation.includes(userId)){
+    return  generate_participant_page();
+  }else{
+    return generate_non_participant_page();
+  }
 }
 
 const styles = StyleSheet.create({
