ed15f41ae71f95a07a9b564bbebde85d4340de19
swaqif02 <92268002+swaqif02@users.noreply.github.com>
2022-04-30 10:46:20 +0000

    mdofi

--- front_eve/app/screens/SignInScreen.js
+++ front_eve/app/screens/SignInScreen.js
@@ -31,7 +31,7 @@ const SignInScreen = ({ navigation }) => {
   var status = 0;
   const loginData = async () => {
     if(data.isValidUser && data.isValidPassword && data.password!="" && data.email!=""){
-      fetch("http://169.254.3.246:3000/login",{
+      fetch("http://192.168.56.1:3000/login",{
         method: "POST",
         headers: { "content-type": "application/json" },
         body: JSON.stringify({ email: data.email, password: data.password }),
