29792d0ab504295edf20d7bd2b142fecaf1b6317
swaqif02 <92268002+swaqif02@users.noreply.github.com>
2022-05-03 10:24:54 +0200

    update

--- front_eve/app/config/uploadImage.js
+++ front_eve/app/config/uploadImage.js
@@ -10,7 +10,7 @@ export default function UploadImage( {imgProfil,id}) {
 
   const [img, setImg] = React.useState(imgProfil)
 const uploadImg = (img)=>{
-  fetch('http://10.43.8.247:3000/editImageProfil',{
+  fetch('http://10.43.11.197:3000/editImageProfil',{
       method: "POST",
       headers: {'content-type': 'application/json'},
       body: JSON.stringify({
